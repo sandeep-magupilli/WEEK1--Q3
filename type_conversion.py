b1ede660ae500a71543cc808e24481ab265e0b24
sandeep-magupilli <24331a05g5@mvgrce.edu.in>
2025-12-19 21:37:05 +0530

    Add files via upload

---
+++ type_conversion.py
@@ -0,0 +1,16 @@
+# Rollnumber: 24331A04G5
+
+# 1.write a python program  to illustrate type conversion functions.
+
+a=input("enter a value:")
+b=input("enter b value:")
+
+a=int(a)
+b=float(b)
+
+#type  conversion
+
+sum=int(a+b)
+print("sum of two numbers is:",sum)
+print("sum is:",type(sum))
+
